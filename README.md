a85623a4a2650ea8a331b9c1a3782875e79e72e2
SolidSnake <solid@snake>
2022-06-21 13:58:21 -0400

    first release

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# cardano-node-docker-iohk
+# Running `cardano-node` and `cardano-cli` using Docker container in less than 5 minutes!
+
+## Running container :smiley:
+You can run a container from the latest iohk cardano-node image.
+
+```
+./docker-run-container.sh
+```
+```
+❯  docker ps
+CONTAINER ID   IMAGE                      COMMAND                  CREATED         STATUS         PORTS     NAMES
+b0081a7ef80f   inputoutput/cardano-node   "cardano-node run --…"   5 seconds ago   Up 4 seconds             pedantic_varahamihira
+``
---
+++ docker-run-container.sh
@@ -0,0 +1,85 @@
+
+get_config_files(){
+read -p "Download testnet config files? this will remove the current config files (y/n) : " ans
+if [[ ${ans} == "y" ]]; then
+    echo -e "\n- Removing configuration files\n"
+    rm -rv configuration
+    echo -e "\n- Downloading configuration files\n"
+    URL_CONFIG_FILES=$(curl -s https://api.github.com/repos/input-output-hk/cardano-node/releases/latest | jq -r .body | grep 'Configuration files' | sed 's/\(- \[Configuration files\]\)//' | tr -d '()\r' | sed 's/\/index\.html//')
+    wget -P ${PWD}/configuration $URL_CONFIG_FILES/testnet-config.json \
+    $URL_CONFIG_FILES/testnet-byron-genesis.json \
+    $URL_CONFIG_FILES/testnet-shelley-genesis.json \
+    $URL_CONFIG_FILES/testnet-alonzo-genesis.json \
+    $URL_CONFIG_FILES/testnet-topology.json
+    echo -e "\n- Updating configurationfiles .. changing stdout logs to a file log destination \n"
+    sed -i 's/StdoutSK/FileSK/' ${PWD}/configuration/testnet-config.json
+    sed -i 's/stdout/\/logs\/node.log/' ${PWD}/configuration/testnet-config.json
+fi
+}
+
+check_missing_files(){
+file_missing=false
+if ! [[ -e ${PWD}/configuration/testnet-config.json ]]; then
+    echo -e "\nError - testnet-config.json file missing"
+    exit
+fi
+if ! [[ -e ${PWD}/configuration/testnet-byron-genesis.json ]]; then
+    echo -e "\nError - testnet-byron-genesis.json file missing"
+    exit
+fi
+if ! [[ -e ${PWD}/configuration/testnet-shelley-genesis.json ]]; then
+    echo -e "\nError - testnet-shelley-genesis.json file missing"
+    exit
+fi
+if ! [[ -e ${PWD}/configuration/testnet-alonzo-genesis.json ]]; then
+    echo -e "\nError - testnet-alonzo-genesis.json file missing"
+    exit
+fi
+if ! [[ -e ${PWD}/configuration/testnet-topology.json ]]; then
+    echo -e "\nError - testnet-topology.json file missing"
+    exit
+fi
+if ! [[ -e ${PWD}/env ]]; then
+    echo -e "\nError - env file missing"
+    exit
+fi
+}
+
+get_config_files
+check_missing_files
+
+echo -e "\n- Found configuration files :\n$(ls configuration) \n"
+
+HOSTADDR="0.0.0.0"
+PORT="6000"
+TOPOLOGY="/configuration/testnet-topology.json"
+CONFIG="/configuration/testnet-config.json"
+DBPATH="/db"
+SOCKETPATH="/db/node.socket"
+
+echo -e "- Docker container will run cardano-node with the following parameters :\nhost : ${HOSTADDR}\nport: ${PORT}\ntopology: ${TOPOLOGY}\nconfig: ${CONFIG}\ndbpath: ${DBPATH}\nsocketpath: ${SOCKETPATH}\n"
+echo -e "- Docker container will bind the following directories :\n$PWD/configuration/ \n$PWD/db/ \n$PWD/logs/ \n"
+
+docker run  -d \
+    -v $PWD/configuration/:/configuration \
+    -v $PWD/db/:/db \
+    -v $PWD/logs/:/logs \
+    --env-file env \
+    --entrypoint cardano-node \
+    inputoutput/cardano-node \
+    run \
+    --topology ${TOPOLOGY} \
+    --database-path ${DBPATH} \
+    --socket-path ${SOCKETPATH} \
+    --host-addr ${HOSTADDR} \
+    --port ${PORT} \
+    --config ${CONFIG}
+
+# * -v $PWD/configuration/:/configuration - bind mount the configuration directory from the host in the container into the /configuration directory
+# * -v $PWD/db/:/db \ - bind mount the db directory from the host in the container into the /db directory
+# * -v $PWD/logs/:/logs - bind mount the logs directory from the host in the container into the /logs directory
+# * --env-file env \ - set environment variables from env file
+# --entrypoint cardano-node \ - the main command when running the container
+# inputoutput/cardano-node \ - the official iohk cardano-node image to use
+#  run - cardano-node option to run the node
+# the rest are just cardano-node parameters
